ed18c4efd2b5ef08e3b99e1594c55aea7be3d34c
Pich09 <120447516+Pich09@users.noreply.github.com>
2024-04-09 10:33:04 +0700

    Add files via upload

---
+++ HashTable/HashKey/Hash.py
@@ -0,0 +1,26 @@
+class HashValue:
+    
+    def __init__(self):
+        self.Size = 10
+        self.arr = [None for i in range(self.Size)]
+        
+    def give_index(self, get_key: str):
+        h = 0
+        for character in get_key:
+            h += ord(character)
+        return h % (self.Size)
+
+    def add_to_table(self, key: str, value: int):
+        index = self.give_index(key)
+        if not self.arr[index]:
+            self.arr[index] = value
+        else: 
+            self.arr[index + 1] = value
+            
+    def get_by_key(self, key: str):
+        index = self.give_index(key) 
+        return self.arr[index]
+    
+    def delete_item(self, key: str):
+        index = self.give_index(key)
+        self.arr[index] = None
---
+++ HashTable/HashTable.py
@@ -0,0 +1,21 @@
+from HashKey import Hash
+
+hash = Hash.HashValue()
+
+#create the hashtable by giving three elements
+hash.add_to_table("Hey", 13)
+hash.add_to_table("Hello", 15)
+hash.add_to_table("Stree", 19)
+hash.add_to_table("Monday", 54)
+hash.add_to_table("Tuesday", 46)
+hash.add_to_table("Dog", 8)
+hash.add_to_table("Cat", 56)
+hash.add_to_table("Monkey", 23)
+hash.add_to_table("Thursday 4", 435)
+hash.add_to_table("Why", 16)
+
+print("HashTable: ", hash.arr)
+print("Key: ", hash.get_by_key("Hey"))
+
+#hash.delete_item("Hello")
+#print("HashTable: ", hash.arr)
